a330624fa219cc6d9aae136a5aa5039e6200af80
Raúl <Ral@DESKTOP-QGD8SSM>
2018-10-26 17:44:14 +0100

    Kata 4 Versión 2. Utilizamos el modelo CIPO.

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -1,6 +1,7 @@
 package kata4.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import kata4.model.Histogram;
 import kata4.model.Mail;
@@ -10,11 +11,35 @@ import kata4.view.MailListReader;
 
 public class Kata4 {
     
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String fileName = "email.txt";
-        List<Mail> mailList = new MailListReader().read(fileName);
-        Histogram<String> histogram = new MailHistogramBuilder().build(mailList);
-        new HistogramDisplay(histogram).execute();
+    private final String fileName;
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    
+    public Kata4(String fileName){
+        this.fileName = fileName;
+        mailList = new ArrayList<>();
     }
     
+    public static void main(String[] args) throws IOException{
+        Kata4 kata4 = new Kata4("email.txt");
+        kata4.execute();
+    }
+
+    private void execute() throws IOException{
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws IOException {
+        mailList = new MailListReader().read(fileName);
+    }
+
+    private void process() {
+        histogram = new MailHistogramBuilder().build(mailList);
+    }
+
+    private void output() {
+        new HistogramDisplay(histogram).execute();
+    }
 }
